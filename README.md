36acee26e3f21358e4894d870336bc3177fd3bee
huanggengzhong <915835528@qq.com>
2020-02-28 16:31:21 +0800

    发布npm包

---
+++ README.md
@@ -0,0 +1,23 @@
+# gz-project
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+# 使用方式:
+```js
+ <vue-count-up :end="2500" :duration="2.5"></vue-count-up>
+```
+
+# 作者联系方式
+https://github.com/huanggengzhong
---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h3>gengzhong-component</h3>
+    <span ref="countup"></span>
+  </div>
+</template>
+
+<script>
+// 使用方式:
+// <vue-count-up :end="2500" :duration="2.5"></vue-count-up>
+import { CountUp } from "countup.js";
+export default {
+  name: "Countupdemo",
+  props: {
+    end: {
+      type: Number,
+      default: 2020
+    },
+    decimal: {
+      type: Number,
+      default: 0
+    },
+    duration: {
+      type: Number,
+      default: 2.5
+    },
+    options: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      numAnim: null
+    };
+  },
+  mounted() {
+    this.initCountUp();
+  },
+  methods: {
+    initCountUp() {
+      this.numAnim = new CountUp(this.$refs.countup, 
+                           this.end,
+                           this.decimal,
+                           this.duration,
+                           this.options
+      );
+      this.numAnim.start();
+    }
+  }
+};
+</script>
+
+<style></style>
